ea2913bd0e6133a9ee26008bcae808e192ced587
François Guilleux <guilleux.francois@gmail.com>
2022-12-12 00:10:22 +0100

    yolo

---
+++ README.md
@@ -0,0 +1,11 @@
+Pour créer des couleurs neons avec css:
+https://css-tricks.com/how-to-create-neon-text-with-css/
+(Pourquoi pas créer un effet de néon qui clignote/déconne)
+
+Magnetic Button:
+https://www.youtube.com/watch?v=lQMadNmdxYA
+
+
+
+The code formatting is available in Visual Studio Code through the following shortcuts:
+    On Mac Shift + Option + F
--- script.js
+++ script.js
@@ -13,39 +13,48 @@ Algorithme:
 document.addEventListener('DOMContentLoaded', (event) => {// When Dom is completely loaded
     console.log(event);
     var getRandomNumber = (min, max) => Math.random() * (max - min) + min;
+    // Declarations for pupilsMovement()
+    let pupilsMoveSheet = document.createElement('style');
+    // Declarations for eyesBlink()
+    let topLids = document.getElementsByClassName('topEyeLid');
+    let bottomLids = document.getElementsByClassName('bottomEyeLid');
+    // Declarations for levitate()
+    const robotHead = document.getElementById('machineCOPY');
+    const robotShadow = document.getElementById('shadow');
+    let levitationSheet = document.createElement('style');
+    // Declarations for speaks()
+    const speakBars = document.getElementById('speakBars').children;
+    let speakSheet = document.createElement('style');
 
     function pupilsMovement() {
         // Creating random eye movement via css transition (First Math.random is duration, second is delay)
-        let transition = "transform " + Math.random() * 3 + "s " + Math.random() * 3 + "s";
+        let pupilsTransition = "transform " + Math.random() * 3 + "s " + Math.random() * 3 + "s";
         let negativeX = Math.random() < 0.5 ? "-" : "";
         let negativeY = Math.random() < 0.5 ? "-" : "";
-        let move = ".move { transform: translate(" + negativeX + Math.random() * 2 + "%," + negativeY + Math.random() * 2 + "%); }"
-        let sheetPupilsMove = document.createElement('style');
-        // Inserting newly created move class in a new stylesheet for the transition
-        sheetPupilsMove.innerHTML = move;
-        document.body.appendChild(sheetPupilsMove);
+        let pupilsMove = ".pupilsMove { transform: translate(" + negativeX + Math.random() * 2 + "%," + negativeY + Math.random() * 2 + "%); }"
+        
+        // Inserting newly created pupilsMove class in a new stylesheet for the transition
+        pupilsMoveSheet.innerHTML = pupilsMove;
+        document.body.appendChild(pupilsMoveSheet);
         // Applying transition to the pupils elements
         let pupils = document.getElementsByClassName('pupils');
         for (const elem of pupils) {
             // elem.addEventListener("mozTransitionEnd", console.log(elem.tagName + ' is DONE!!!'), false);  
-            elem.style.transition = transition;
+            elem.style.transition = pupilsTransition;
             elem.ontransitionend = () => {// For later: Handle browser compatibility (ex:webkittransitionend)
                 pupilsMovement();// <--- It retriger the pupils movement when finished 👍
             };
-            elem.classList.add("move");
+            elem.classList.add("pupilsMove");
         }
     }
     
     function eyesBlink() {
         // Generating css animations
-        if (Math.random() < 0.6) { // Trigger only x% of the time
+        if (Math.random() < 0.6) { // if statement make it trigger only x% of the time
             let blinkDuration = getRandomNumber(0.1, 0.4).toString();
             let blinkDelay = getRandomNumber(0.1, 4.5).toString();
             let animBlinkTop = `blink-bottom-eye-lid ${blinkDuration}s ${blinkDelay}s 2 alternate ease-in-out`;
             let animBlinkBottom = `blink-top-eye-lid ${blinkDuration}s ${blinkDelay}s 2 alternate ease-in-out`;
-
-            let topLids = document.getElementsByClassName('topEyeLid');
-            let bottomLids = document.getElementsByClassName('bottomEyeLid');
             
             console.warn("!!!! eyesBlink has been called !!!!!");
             console.warn(animBlinkBottom);
@@ -53,7 +62,7 @@ document.addEventListener('DOMContentLoaded', (event) => {// When Dom is complet
             
             lidsMove = (lids) => {
                 /* css animation wont retrigger unless the element is detroyed and recreated,
-                hence the removeChild and appendChild */
+                hence removeChild and appendChild */
                 for (elem of lids) {
                     let parent = elem.parentNode;
                     parent.removeChild(elem);
@@ -69,36 +78,28 @@ document.addEventListener('DOMContentLoaded', (event) => {// When Dom is complet
         }
     }
 
-    function levitate () { // Maybe we can avoid to recall document.getElement each call
+    function levitate () {
         console.warn("--- levitate called ---")
-        const robotHead = document.getElementById('machineCOPY');
-        const robotShadow = document.getElementById('shadow');
 
         const levitationTransition = "all 2s ease-in-out";// To randomize
         robotHead.style.transition = robotShadow.style.transition = levitationTransition;
 
-
         let levitationMove = `
-        .levitationMove { transform: translate(5%, -12.5%) rotate(1.7deg); }
-        .shadowMove { transform: scaleX(1.2); }`;
-        let sheetLevitation = document.createElement('style');
-        sheetLevitation.innerHTML = levitationMove;
-        document.body.appendChild(sheetLevitation);
+        .levitationMove { transform: translateY(-12.5%) }
+        .shadowMove { transform: scaleX(1.2); }`;// To randomize
+        
+        levitationSheet.innerHTML = levitationMove;
+        document.body.appendChild(levitationSheet);
         
         robotHead.classList.add('levitationMove');
         robotShadow.classList.add('shadowMove');
 
         robotHead.ontransitionend = () => {
             console.log("Levitaitiion animation END");
-            // let parent = robotHead.parentNode;
-            // parent.removeChild(robotHead);
-            // parent.appendChild(robotHead);
-            // robotHead.style.transition = levitationTransition;
-
             levitationMove = `
-            .levitationMoveDown { transform: translate(0%, 0%) rotate(-3.7deg); }
+            .levitationMoveDown { transform: translate(0%, 0%) }
             .shadowMoveDown { transform: scaleX(1); }`;
-            sheetLevitation.innerHTML = levitationMove;
+            levitationSheet.innerHTML = levitationMove;
             robotHead.classList.add('levitationMoveDown');
             robotShadow.classList.add('shadowMoveDown');
 
@@ -106,49 +107,57 @@ document.addEventListener('DOMContentLoaded', (event) => {// When Dom is complet
         }
     }
 
-    function speak() {
+    function speak() {// https://css-tricks.com/scale-svg/ <---------
         // console.log(document.getElementById('speakBars'));
-        let barNum = 0;
-        let [bass, middle, high] = [[], [], []]; // One lijne declaration of multiple arrays 🤙
-        // let bass = middle = high = [];
-        // let bass = [];
-        // let middle = [];
-        // let high = [];
-        for (bar of document.getElementById('speakBars').children) {
-            console.log("----> ");
-            // console.log(bar);
-            if (barNum < 3) bass.push(bar)
-            else if (barNum >= 3 && barNum < 6) middle.push(bar)
-            else high.push(bar)
-            // if (barNum < 3) {
-            //     console.log('BASS');
-            //     bass.push(bar);
-            // } else if (barNum >= 3 && barNum < 6) {
-            //     console.log('MIDDLE');
-            //     middle.push(bar);
-            // } else {
-            //     console.log('HIGH');
-            //     high.push(bar);
-            // }
+        // let barNum = 0;
+        // let [bass, middle, high] = [[], [], []]; // One line declaration of multiple arrays 🤙
 
-            ++barNum;
-        }
+        // for (bar of speakBars) {
+        //     console.log("----> ");
+        //     // console.log(bar);
+        //     if (barNum < 3) bass.push(bar)
+        //     else if (barNum >= 3 && barNum < 6) middle.push(bar)
+        //     else high.push(bar);
+        //     // if (barNum < 3) {
+        //     //     console.log('BASS');
+        //     //     bass.push(bar);
+        //     // } else if (barNum >= 3 && barNum < 6) {
+        //     //     console.log('MIDDLE');
+        //     //     middle.push(bar);
+        //     // } else {
+        //     //     console.log('HIGH');
+        //     //     high.push(bar);
+        //     // }
 
-        
+        //     ++barNum;
+        // }
+        let masterBar = ('speakBar' + 10).toString();
+        masterBar = document.getElementById(masterBar);
+        console.log(masterBar);
+        let barTransition = "3s";
+        let barMove = ".barMove { transform: scaleY(1.2); }"
+        // masterBar.style.transition = barTransition;
+        speakSheet.innerHTML = barMove;
+        document.body.appendChild(speakSheet);
+        masterBar.classList.add('barMove');
     }
-    speak();
+    // speak();
 
     function grabAntenna() {
         /* Must be able to grab antenna by clicking (or swiping on touchscreens) and make it 
-        move and maybe drag the robot a little... */
+        move and maybe drag the robot a little... Check magnetic button video link in README
+        for inspiration */
     }
 
     // Pour randomiser le clignement il faut créer un num random pour le setInterval, et le passer à eyesBlink
     // pour que le delay ne dépasse pas le num random (faire en sorte que le clignement soit finit avant la fin
     // du setInterval sinon les paupières se remettent en position ouverte d'un coup).
-    var eyesBlinkVar = setInterval(eyesBlink, 5000);
+    setInterval(eyesBlink, 5000);
 
     levitate();
 
     pupilsMovement();
+
+    /* Generating Stars */
+
 });
